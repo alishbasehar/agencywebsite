1c862fd027b7363118470a126767989fbb87d3d9
alishbasehar <alishbasehar505@gmail.com>
2025-12-25 21:47:56 +0500

    agencu repo created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 py-4 sticky top-0 z-20 backdrop-blur-2xl font-medium bg-white/50 dark:bg-gray-900/70'>Navbar</div>
+  )
+}
+
+export default Navbar
